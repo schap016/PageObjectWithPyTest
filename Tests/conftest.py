d7a5d36c191fb8ec0458c151e40776c71cf57f84
schap016 <samantha.chapa@gmail.com>
2019-06-17 13:25:00 -0700

    Update conftest.py

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -10,7 +10,7 @@ import pytest
 
 @pytest.fixture()
 def driver():
-    driver_path = os.path.join(myPath, "TestResources/drivers/chromedriver")
+    driver_path = os.path.replace("/Tests","/TestResources/drivers/chromedriver")
     driver = webdriver.Chrome(driver_path)
     driver.implicitly_wait(5)
     driver.set_window_size(1260, 1080)
