0f8003653236ccca69bfff10422082281e9eb971
schap016 <samantha.chapa@gmail.com>
2019-06-19 22:16:45 -0400

    Update conftest.py

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -2,30 +2,33 @@ import os
 import sys
 
 myPath = os.path.dirname(os.path.abspath(__file__))
+print(myPath)
 sys.path.insert(0, myPath + '/../')
 from selenium import webdriver
 from PageObjects.homePage import HomePage
 import pytest
-import platform
+from selenium.webdriver.chrome.options import Options
+import platform 
 platform = platform.system()
 
 
 @pytest.fixture()
 def driver():
-    driver_path = os.path.join(myPath, "../TestResources/drivers/chromedriver")
-    #driver_path = os.path.join(myPath, "../TestResources/drivers/chromedriver")
-    if(platform!='Windows'):
-        driver_path = driver_path.replace("/Tests/TestResources/drivers/chromedriver","/TestResources/drivers/chromedriver")
-    driver = webdriver.Chrome(driver_path)
-    driver.maximize_window()
-    driver.implicitly_wait(3)
-    driver.set_window_size(1260, 1080)
+    if platform == 'Windows':
+        driver_path = os.path.join(myPath, "../TestResources/drivers/chromedriver")
+        driver = webdriver.Chrome(driver_path)
+        driver.implicitly_wait(5)
+        driver.set_window_size(1260, 1080)
+       
+    elif platform == 'Linux':         
+        driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver/chromedriver")    
+        driver.implicitly_wait(5)
+        driver.set_window_size(1260, 1080)
     return driver
 
 @pytest.fixture()
 def home_page(driver):
     home_page = HomePage(driver)
     return home_page
-
 
 
