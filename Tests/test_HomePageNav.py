e5b404ff6bb248e7b0b7f84e53de629bacd68feb
schap016 <samantha.chapa@gmail.com>
2019-06-24 13:04:19 -0400

    Update test_HomePageNav.py

--- Tests/test_HomePageNav.py
+++ Tests/test_HomePageNav.py
@@ -10,22 +10,31 @@ baseUrl = "https://www.phptravels.net/"
 
 
 @pytest.mark.dependency()
-def test_home_page_navigation_to_PHPTravels(driver,home_page):
+def test_home_page_navigation_to_PHPTravels(driver,home_page,screenshot_path):
     driver.get(baseUrl)
     home_page_nav = home_page.is_title_matches()
     if home_page_nav:
        assert True
+       driver.close()
     else:
+       driver.save_screenshot(screenshot_path + "test_home_page_navigation_to_PHPTravels.png")
+       driver.close()
        assert False
-    driver.close()
+
 
 
 @pytest.mark.dependency(depends=["test_home_page_navigation_to_PHPTravels"])
-def test_verify_booking_tabs(driver,home_page):
+def test_verify_booking_tabs(driver,home_page,screenshot_path):
     driver.get(baseUrl)
     _primary_tabs_verified = home_page.verify_primary_tabs_homePage()
-    assert str(_primary_tabs_verified)=="True"
-    driver.close()
+    if _primary_tabs_verified:
+        driver.close()
+        assert True
+    else:
+
+        driver.save_screenshot(screenshot_path + "test_verify_booking_tabs.png")
+        driver.close()
+        assert False
 
 
 
