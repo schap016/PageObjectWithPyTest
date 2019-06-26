eddb14b96ea9334bc6f3bc7eb18ee3530d47a8e1
schap016 <samantha.chapa@gmail.com>
2019-06-26 12:04:52 -0400

    Update conftest.py

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -39,5 +39,96 @@ def screenshot_path():
         screeenshot_folder_path =myPath.replace("/Tests", "/TestResources/failed_tests_screens/")
     return screeenshot_folder_path
 
+@pytest.fixture(scope="session", autouse=True)
+def my_own_session_run_at_beginning(request):
+    print('\nIn my_own_session_run_at_beginning()')
+
+    def my_own_session_run_at_end():
+        print('In my_own_session_run_at_end()')
+        # Generate HTML file report
+
+
+
+
+        #READ CONTACTS
+        contacts = []
+        contacts_path = os.path.join(myPath, "../TestResources/contacts.txt")
+        read = open(contacts_path, "r")
+        contents = read.readlines()
+        for line in contents:
+            line = line.replace('\n', '')
+            contacts.append(line)
+
+        #GET PASSWORD FILE PATH
+        if platform == "Windows":
+            password_file = os.path.join(myPath, "../TestResources/password.txt")
+        if platform == "Linux":
+            password_file = myPath.replace("/Tests", "/TestResources/password.txt")
+
+        print("report path")
+        #print(testReport_path)
+        # read stored password
+        with open(password_file, 'rb') as f:
+            password = f.read()
+
+        # encrypt password
+        key = Fernet.generate_key()
+        fernet = Fernet(key)
+        encrypted_password = fernet.encrypt(password)
+
+        # decrypt password
+        fernet_decrypt = Fernet(key)
+        decrypted_password = fernet_decrypt.decrypt(encrypted_password)
+
+        PASSWORD = decrypted_password.decode()
+        MY_ADDRESS = "samanthachapa31990@gmail.com"
+        print("Email Address" + MY_ADDRESS)
+        #print("Password" + PASSWORD)
+
+        if platform == "Windows":
+            reports_file_path = os.path.join(myPath, "../reports/report.html")
+        if platform == "Linux":
+            reports_file_path = myPath.replace("/Tests", "/reports/report.html")
+        try:
+            s = smtplib.SMTP(host='smtp.gmail.com', port=587)
+            s.ehlo()
+            s.starttls()
+            s.login(MY_ADDRESS, PASSWORD)
+            print(s.noop())
+            for email in contacts:
+                msg = MIMEMultipart()  # create a message
+
+                # add in the actual person name to the message template
+                message = "Hello, test email"
+
+                # setup the parameters of the message
+                msg['From'] = MY_ADDRESS
+                msg['To'] = email
+                msg['Subject'] = "This is TEST"
+
+                # add in the message body
+                msg.attach(MIMEText(message))
+
+                # add attachments
+                filename = "report.html"
+                attachment = open(reports_file_path, "rb")
+
+                part = MIMEBase('application', 'octet-stream')
+                part.set_payload((attachment).read())
+                encoders.encode_base64(part)
+                part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
+                msg.attach(part)
+
+                # send the message via the server set up earlier.
+                s.send_message(msg)
+                del msg
+            s.quit()
+        except:
+            print("Exception occurred")
+
+
+    request.addfinalizer(my_own_session_run_at_end)
+
 
 
